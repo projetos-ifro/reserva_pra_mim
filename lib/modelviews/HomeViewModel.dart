085f6351b73cdbe937a18f117b66ebfba70d2609
= <128653411+thauan12@users.noreply.github.com>
2023-10-15 22:05:13 -0300

    Ajustando navigationBar

---
+++ lib/modelviews/HomeViewModel.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../views/pagesnavigation/historico.dart';
+import '../views/pagesnavigation/inicio.dart';
+import '../views/pagesnavigation/progresso.dart';
+
+class HomeViewModel {
+  Widget buildPage(int index) {
+    switch (index) {
+      case 0:
+        return ProgessoPage();
+      case 1:
+        return InicioPage();
+      case 2:
+        return HistoricoPage();
+      default:
+        return Container();
+    }
+  }
+}
---
+++ lib/views/pagesnavigation/progresso.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProgessoPage extends StatelessWidget {
+  const ProgessoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
